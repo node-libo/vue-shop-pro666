daf0fed97a496af5f9c5a14944645f9ebbd12626
libo <1036708552@qq.com>
2019-02-13 22:37:46 +0800

    登录功能开发完毕

--- src/main.js
+++ src/main.js
@@ -2,6 +2,24 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// 引入css样式全局控制
+import './assets/css/global.css'
+
+// 引入element-ui
+import ElementUI from 'element-ui'
+
+// 引入“图标库”css样式文件
+import './assets/fonts/iconfont.css'
+
+// 引入axios
+import axios from 'axios'
+// 给axios做配置(访问根地址，Vue之$http成员)
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1'
+Vue.prototype.$http = axios
+
+// 注册全部的element组件
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
